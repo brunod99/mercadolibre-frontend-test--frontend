7877baee96f850f6d295eb0d87eee4c0c5927dbb
Bruno <brunodigonzelli@git.com>
2020-09-09 03:52:25 -0300

    #9 - add validation splash home

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,13 +1,21 @@
 import React from "react";
+import { useSelector } from "react-redux";
 import Splash from "../components/Splash";
+import { IInitialProductsState } from "../types/Redux/Products";
 const Home: React.FC = () => {
+  const { products } = useSelector(
+    (state: { products: IInitialProductsState }) => state.products
+  );
+
   return (
     <main className="home overflow-hidden">
-      <Splash isAnimated>
-        <h1 className="text-white f-weight-bold">
-          ¡Bienvenido a Mercado Libre!
-        </h1>
-      </Splash>
+      {products.length === 0 && (
+        <Splash isAnimated>
+          <h1 className="text-white f-weight-bold">
+            ¡Bienvenido a Mercado Libre!
+          </h1>
+        </Splash>
+      )}
     </main>
   );
 };
