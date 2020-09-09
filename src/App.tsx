ea544b94ab757a4c6874b6bbfc61bfb0598a7d2a
Bruno <brunodigonzelli@git.com>
2020-09-09 02:36:25 -0300

    #8 - Create and add history

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,25 @@
 import React from "react";
 import { Router, Switch, Route } from "react-router-dom";
-import { createBrowserHistory } from "history";
 import Header from "./components/Header/Header";
 import Home from "./pages/Home";
 import Products from "./pages/Products/Products";
 import ProductSingle from "./pages/Products/ProductSingle";
 import { Provider } from "react-redux";
 import store from "./redux/store";
-
-const history = createBrowserHistory();
+import history from "./history";
 
 function App() {
   return (
-    <Router history={history}>
-      <Provider store={store}>
-        <Header history={history} />
+    <Provider store={store}>
+      <Router history={history}>
+        <Header />
         <Switch>
           <Route exact path="/" component={Home} />
           <Route exact path="/items" component={Products} />
           <Route exact path="/items/:id" component={ProductSingle} />
         </Switch>
-      </Provider>
-    </Router>
+      </Router>
+    </Provider>
   );
 }
 
